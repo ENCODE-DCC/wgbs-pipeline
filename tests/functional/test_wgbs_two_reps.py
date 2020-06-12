f2c8e2cf1b32c8cac757f5b05b1f2136ddb23e5b
Paul Sud <paulsud@stanford.edu>
2020-06-12 14:27:32 -0700

    use proper float comparison

--- tests/functional/test_wgbs_two_reps.py
+++ tests/functional/test_wgbs_two_reps.py
@@ -1,10 +1,13 @@
+import json
+import math
+
 import pytest
 
 
 @pytest.mark.workflow("test_wgbs_two_reps")
 def test_wgbs_two_reps_check_pearson_qc(workflow_dir, test_data_dir):
     with open(test_data_dir / "bed_pearson_correlation_qc.json") as f:
-        expected = f.read()
+        expected_qc = json.load(f)
 
     result_path = next(
         (workflow_dir / "test-output").glob(
@@ -12,5 +15,8 @@ def test_wgbs_two_reps_check_pearson_qc(workflow_dir, test_data_dir):
         )
     )
     with open(result_path) as f:
-        result = f.read()
-    assert result == expected
+        result_qc = json.load(f)
+
+    result = result_qc["pearson_correlation"]["pearson_correlation"]
+    expected = expected_qc["pearson_correlation"]["pearson_correlation"]
+    assert math.isclose(result, expected, rel_tol=1e-5)
