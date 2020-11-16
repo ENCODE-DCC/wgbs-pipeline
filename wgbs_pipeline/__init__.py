1c5dc976b33f3e55b591d163261b679a0624e805
Paul Sud <paulsud@stanford.edu>
2020-11-16 09:47:39 -0800

    update to 1.1.1

--- wgbs_pipeline/__init__.py
+++ wgbs_pipeline/__init__.py
@@ -1,5 +1,5 @@
 __title__ = "wgbs-pipeline"
-__version__ = "1.1.0"
+__version__ = "1.1.1"
 __description__ = "Python scripts required for running ENCODE WGBS pipeline."
 __url__ = "https://github.com/ENCODE-DCC/wgbs-pipeline"
 __uri__ = __url__
